8d5b495d15e0cc0bec4140b3421f3436702df0de
Александр Биктеев <bikteev@mail.ru>
2021-08-02 17:15:54 +0900

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by albik on 02.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
